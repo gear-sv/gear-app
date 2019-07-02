dbd66c7f4fedd90af053c391e0d15438daf9ac53
Sean Pollock <seanpollock3344@gmail.com>
2019-07-02 18:16:53 -0400

    grid layout

--- src/Main.js
+++ src/Main.js
@@ -1,22 +1,46 @@
 import React, { Component } from "react"
-import { Grid, MenuList, MenuItem, Avatar, TextField } from "@material-ui/core"
+import { AppBar, Grid, MenuList, MenuItem, Avatar, TextField, Paper, Button } from "@material-ui/core"
+import { ThemeProvider, MuiThemeProvider, getMuiTheme } from "@material-ui/styles"
+import CssBaseline from "@material-ui/core/CssBaseline"
+import theme from "./theme.js"
 
 export default class Main extends Component {
   render = () => (
-      <Grid container spacing={3} style={{height: "100%" }}>
-        <Grid item xs={2} style={{ backgroundColor: "rgba(0, 0, 0, 0.87)", height: "100%" }}>   
-          <img alt="gearSV" src={require("./gearsv.png")} />
+    <ThemeProvider theme={theme}>
+      <CssBaseline />
+      <Grid container spacing={3}>
+        <Grid item xs={12} style={{ height: 60 }}>
+          <AppBar>
+            <div style={{ height: 60 }}>
+            </div>
+          </AppBar>
         </Grid>
-        <Grid item xs={10} style={{ backgroundColor: "#121212" }}>
-          <Grid item xs={5} style={{ backgroundColor: "#333", height: "50%" }}>
-            <TextField 
-              id="address"
-              label="String"
-              margin="normal"
-              style={{ color: "white" }}
-            />
-          </Grid>
+        <Grid item xs={12} style={{ margin: 20 }}>
+          <Paper style={{ width: "100%", height: "100" }}>
+            <Grid xs={12} container style={{ padding: 10 }}>
+              <Grid item xs={2}>
+                <h2 style={{ color: "white" }}>Token Contract</h2>
+              </Grid>
+              <Grid item xs={3}>
+                <Button>
+                  SetOwner
+                </Button>
+                <Button>
+                  Mint
+                </Button>
+                <Button>
+                  Transfer
+                </Button>
+              </Grid>
+            </Grid>
+          </Paper>
+        </Grid>
+        <Grid item xs={12} style={{ padding: 10, margin: 20 }}>
+          <Paper style={{ padding: 10 }}>
+            <h2>Market</h2>
+          </Paper>
         </Grid>
       </Grid>
+    </ThemeProvider>
   )
 }
